653e62a6a4a7f8a132646b83049259a2fb34257d
Maksym-Babich <maxus1042@gmail.com>
2023-04-26 20:55:21 +0300

    selector for filtered contacts added

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,25 +1,14 @@
 import { useSelector } from 'react-redux';
 import { ContactsList } from 'components/Contacts/Contacts.styled';
 import { Contact } from 'components/Contacts/Contact/Contact';
-import { selectContacts, selectFilterValue } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 
 export function Contacts() {
-  const contacts = useSelector(selectContacts);
-  const filterValue = useSelector(selectFilterValue);
-
-  const getfilteredItems = () => {
-    const filteredItems = contacts.filter(contact => {
-      return (
-        contact.name.toLowerCase().includes(filterValue.toLowerCase()) ||
-        contact.number.includes(filterValue)
-      );
-    });
-    return filterValue ? filteredItems : contacts;
-  };
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ContactsList>
-      {getfilteredItems().map(contact => {
+      {filteredContacts.map(contact => {
         const { name, number, id } = contact;
         return (
           <Contact key={id} content={` ${name}: ${number}`} id={id}></Contact>
